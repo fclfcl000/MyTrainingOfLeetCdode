8377504aaa16fb5c6aed37917707a51f42875edc
fclfcl000@gmail.com <fclfcl000@gmail.com>
2015-10-25 23:38:04 +0800

    1. accepted solution for TwoSum, Problem No.1

--- TwoSum.py
+++ TwoSum.py
@@ -1,12 +1,28 @@
 class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for k, i in enumerate(nums):
-            for z, j in enumerate(nums[k+1:]):
-				if target == i + j:
-					print "index1=%d, index2=%d" % (k + 1, k + 1 + z + 1)
-					return
+	def twoSumd(self, nums, target):
+		"""
+		:type nums: List[int]
+		:type target: int
+		:rtype: List[int]
+		"""
+		ntarget = target
+		if target == 0:
+			target = 1
+		if target < 0:
+			target = 0 - target
+		t = [None] * target
+		for k, v in enumerate(nums):
+			h = v % target
+			if h < 0:
+				h = target + h
+			g = (target - v) % target
+			if g < 0:
+				g = target + hs
+			if t[h] == None:
+				 t[h] = dict()
+			if t[g] != None:
+				for (m, n) in t[g].items():
+					if ntarget == v + n:
+						print("index1=%d, index2=%d" % (m+1, k+1))
+						return (m+1, k+1)
+			t[h][k] = v
